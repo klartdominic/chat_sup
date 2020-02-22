8d430351ff2389a17de6c1775ede628b330d5ac7
klartdominic <klartdominic@gmail.com>
2020-02-22 22:40:02 +0800

    sendbird api

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import CounterApp from './screen/counterApp';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './src/reducers/counterAppReducer';
+import SendBirdSampleScreen from './screen/sendBirdSample';
 
 class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <CounterApp />
+        {/* <CounterApp /> */}
+        <SendBirdSampleScreen />
       </Provider>
     );
   }
